a89d3102a7516a5f23d07dc78b4edc6e68ee5214
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-04-11 13:49:17 +0200

    fix: avoid keyboard input mixin conflict with rsm

--- src/main/java/quoi/mixins/KeyboardInputMixin.java
+++ src/main/java/quoi/mixins/KeyboardInputMixin.java
@@ -2,24 +2,25 @@ package quoi.mixins;
 
 import net.minecraft.client.player.KeyboardInput;
 import net.minecraft.world.entity.player.Input;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import quoi.api.events.KeyEvent;
 import quoi.api.input.MutableInput;
 
 @Mixin(KeyboardInput.class)
 public class KeyboardInputMixin {
-    @Redirect(
+    @Inject(
             method = "tick",
             at = @At(
-                    value = "FIELD",
-                    target = "Lnet/minecraft/client/player/KeyboardInput;keyPresses:Lnet/minecraft/world/entity/player/Input;",
-                    opcode = Opcodes.PUTFIELD
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/entity/player/Input;forward()Z"
             )
     )
-    private void onTick(KeyboardInput instance, Input input) {
+    private void onTick(CallbackInfo ci) {
+        KeyboardInput instance = (KeyboardInput) (Object) this;
+        Input input = instance.keyPresses;
         KeyEvent.Input event = new KeyEvent.Input(input, new MutableInput(input));
         event.post();
         instance.keyPresses = event.getInput().toInput();
