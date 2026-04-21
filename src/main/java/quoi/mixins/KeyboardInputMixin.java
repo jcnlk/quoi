bf93bcb27d8173cfc915f54bcc4708414087466a
pigeonlover1998 <200670258+pigeonlover1998@users.noreply.github.com>
2026-04-21 14:14:32 +0300

    fix: rsm compat
    (cherry picked from commit 219c07e8e10e0d1be90c75cabc2202324662c668)
    (cherry picked from commit e4540e975ddf260d086a6af78177082300a286f9)

--- src/main/java/quoi/mixins/KeyboardInputMixin.java
+++ src/main/java/quoi/mixins/KeyboardInputMixin.java
@@ -1,26 +1,26 @@
 package quoi.mixins;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.player.KeyboardInput;
 import net.minecraft.world.entity.player.Input;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import quoi.api.events.KeyEvent;
 import quoi.api.input.MutableInput;
 
 @Mixin(KeyboardInput.class)
 public class KeyboardInputMixin {
-    @Inject(
+    @WrapOperation(
             method = "tick",
             at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/world/entity/player/Input;forward()Z"
+                    value = "FIELD",
+                    target = "Lnet/minecraft/client/player/KeyboardInput;keyPresses:Lnet/minecraft/world/entity/player/Input;",
+                    opcode = Opcodes.PUTFIELD
             )
     )
-    private void onTick(CallbackInfo ci) {
-        KeyboardInput instance = (KeyboardInput) (Object) this;
-        Input input = instance.keyPresses;
+    private void onTick(KeyboardInput instance, Input input, Operation<Void> original) {
         KeyEvent.Input event = new KeyEvent.Input(input, new MutableInput(input));
         event.post();
         instance.keyPresses = event.getInput().toInput();
