2182c742540845c8b47bdaadff2689a0da0770e9
pigeonlover1998 <200670258+pigeonlover1998@users.noreply.github.com>
2026-04-29 20:55:53 +0300

    fix: etherwarp on vines and room rotations
    (cherry picked from commit 38f456c02e408ee837877e970a6b34c2a9b9e9f9)

--- src/main/kotlin/quoi/utils/VecUtils.kt
+++ src/main/kotlin/quoi/utils/VecUtils.kt
@@ -799,7 +799,7 @@ val blockFlags: IntArray = IntArray(Block.BLOCK_STATE_REGISTRY.size()).apply {
             is SkullBlock, is WallSkullBlock -> true
             is FlowerPotBlock -> true
             is LadderBlock -> true
-            is VineBlock -> true
+//            is VineBlock -> true
             else -> false
         }
 
