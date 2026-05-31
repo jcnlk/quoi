0898148da3b30770786704d4fc5b497a4ee376f3
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-05-31 18:57:11 +0200

    feat(PuzzleSolvers): add boulder solver

--- src/main/kotlin/quoi/utils/render/WorldRenderContextUtils.kt
+++ src/main/kotlin/quoi/utils/render/WorldRenderContextUtils.kt
@@ -151,6 +151,7 @@ fun WorldRenderContext.drawStyledBox(style: String, box: AABB, colour: Colour, f
             drawFilledBox(box, fillColour, depth)
             drawWireFrameBox(box, colour, thickness, depth)
         }
+        "Filled" -> drawFilledBox(box, fillColour, depth)
     }
 }
 
