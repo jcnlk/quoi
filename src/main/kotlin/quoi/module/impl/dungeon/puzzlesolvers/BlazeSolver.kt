be55061f99702f7260f92174f864ddd6c0a54ea5
pigeonlover1998 <200670258+pigeonlover1998@users.noreply.github.com>
2026-06-07 08:07:01 +0300

    fix(blaze): rendering stuff and unsneaking when auto is disabled

--- src/main/kotlin/quoi/module/impl/dungeon/puzzlesolvers/BlazeSolver.kt
+++ src/main/kotlin/quoi/module/impl/dungeon/puzzlesolvers/BlazeSolver.kt
@@ -69,10 +69,10 @@ object BlazeSolver { // todo maybe improve terminator shit some day
         if (!this?.name.equalsOneOf("Lower Blaze", "Higher Blaze") ) return@with reset()
     }
 
-    fun onRenderWorld(ctx: LevelRenderContext, blazeLineNext: Boolean, blazeLineAmount: Int, blazeStyle: String, blazeFirstColour: Colour, blazeSecondColour: Colour, blazeThirdColour: Colour, blazeAllColour: Colour, blazeAnnounce: Boolean, blazeLineWidth: Float, autoReposition: Boolean) {
+    fun onRenderWorld(ctx: LevelRenderContext, blazeLineNext: Boolean, blazeLineAmount: Int, blazeStyle: String, blazeFirstColour: Colour, blazeSecondColour: Colour, blazeThirdColour: Colour, blazeAllColour: Colour, blazeAnnounce: Boolean, blazeLineWidth: Float, auto: Boolean, autoReposition: Boolean) {
         if (!Dungeon.currentRoom?.name.equalsOneOf("Lower Blaze", "Higher Blaze") || blazes.isEmpty()) return
 
-        if (autoReposition) {
+        if (auto && autoReposition) {
             val spots = if (Dungeon.currentRoom?.name == "Higher Blaze") higherSpots else lowerSpots
             spots.forEach { spot ->
                 ctx.drawFilledBox(Dungeon.currentRoom!!.getRealCoords(spot).aabb, colour = Colour.CYAN, depth = true)
@@ -84,7 +84,7 @@ object BlazeSolver { // todo maybe improve terminator shit some day
         if (blazes.isEmpty() && lastBlazeCount == 1) {
             Dungeon.puzzles.find { it == Puzzle.BLAZE }?.status = PuzzleStatus.Completed
             if (blazeAnnounce) ChatUtils.command("pc Blaze done!")
-            if (autoReposition) mc.options.keyShift.isDown = false
+            if (auto && autoReposition) mc.options.keyShift.isDown = false
             lastBlazeCount = 0
             return
         }
