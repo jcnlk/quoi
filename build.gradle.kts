72dea71d74988996462222904a61bb5608eb7829
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-06-07 03:17:43 +0200

    chore: clean up

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,22 +13,30 @@ repositories {
     maven("https://pkgs.dev.azure.com/djtheredstoner/DevAuth/_packaging/public/maven/v1")
 }
 
+base {
+    archivesName.set(property("archives_base_name") as String)
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
 dependencies {
     minecraft("com.mojang:minecraft:${property("minecraft_version")}")
-//    mappings("net.fabricmc:yarn:${property("yarn_mappings")}:v2")
     mappings(loom.officialMojangMappings())
 
     modImplementation("net.fabricmc:fabric-loader:${property("loader_version")}")
     modImplementation("net.fabricmc:fabric-language-kotlin:${property("fabric_kotlin_version")}")
     modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_api_version")}")
-    modRuntimeOnly("me.djtheredstoner:DevAuth-fabric:1.2.1")
-    runtimeOnly("org.apache.httpcomponents:httpclient:4.5.14")
+    modRuntimeOnly("me.djtheredstoner:DevAuth-fabric:${property("devauth_version")}")
+    runtimeOnly("org.apache.httpcomponents:httpclient:${property("httpclient_version")}")
 
-    modImplementation("io.github.classgraph:classgraph:4.8.184")
-    include("io.github.classgraph:classgraph:4.8.184")
+    property("classgraph_version").let {
+        modImplementation("io.github.classgraph:classgraph:$it")
+        include("io.github.classgraph:classgraph:$it")
+    }
 
     property("minecraft_lwjgl_version").let {
-
         modImplementation("org.lwjgl:lwjgl-nanovg:$it")
         include("org.lwjgl:lwjgl-nanovg:$it")
 
@@ -67,8 +75,20 @@ afterEvaluate {
 
 tasks {
     processResources {
+        val modProperties = listOf(
+            "mod_id",
+            "mod_name",
+            "mod_version",
+            "loader_version",
+            "fabric_api_version",
+            "fabric_kotlin_version",
+            "minecraft_version",
+        ).associateWith { providers.gradleProperty(it).get() }
+
+        inputs.properties(modProperties)
+
         filesMatching("fabric.mod.json") {
-            expand(getProperties())
+            expand(modProperties)
         }
     }
 
@@ -80,13 +100,15 @@ tasks {
     }
 
     compileJava {
-        sourceCompatibility = "21"
-        targetCompatibility = "21"
         options.encoding = "UTF-8"
         options.compilerArgs.addAll(listOf("-Xlint:deprecation", "-Xlint:unchecked"))
     }
 }
 
 java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+
     withSourcesJar()
 }
