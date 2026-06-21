61d09848ad19dfcc2fde81325182a595f9876ba7
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-06-21 19:10:18 +0200

    fix(DungeonESP): teammate class glow not working in boss

--- src/main/kotlin/quoi/module/impl/dungeon/DungeonESP.kt
+++ src/main/kotlin/quoi/module/impl/dungeon/DungeonESP.kt
@@ -48,7 +48,7 @@ import quoi.utils.render.drawStyledBox
 object DungeonESP : Module(
     "Dungeon ESP",
     desc = "Highlights various dungeon entities.",
-    area = Island.Dungeon(inClear = true)
+    area = Island.Dungeon()
 ) { // todo recode
     private val teammateClassGlow by switch("Teammate class glow", true, desc = "Highlights dungeon teammates based on their class colour.")
     private val starEsp by switch("Starred mobs")
