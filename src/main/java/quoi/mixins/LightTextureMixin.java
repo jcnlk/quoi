0c0d631117df3ab9962e06ea484dfb70452c0fac
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-07-03 15:21:05 +0200

    fix(RenderOptimiser): fullbright not working in 26.2

--- src/main/java/quoi/mixins/LightTextureMixin.java
+++ src/main/java/quoi/mixins/LightTextureMixin.java
@@ -3,8 +3,10 @@ package quoi.mixins;
 import quoi.module.impl.render.RenderOptimiser;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.client.renderer.LightmapRenderStateExtractor;
+import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Slice;
 
 import static quoi.module.impl.render.RenderOptimiser.should;
 
@@ -15,10 +17,16 @@ public class LightTextureMixin {
             method = "extract",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/world/level/dimension/DimensionType;ambientLight()F"
+                    target = "Lnet/minecraft/util/ARGB;vector3fFromRGB24(I)Lorg/joml/Vector3f;"
+            ),
+            slice = @Slice(
+                    from = @At(
+                            value = "FIELD",
+                            target = "Lnet/minecraft/world/attribute/EnvironmentAttributes;AMBIENT_LIGHT_COLOR:Lnet/minecraft/world/attribute/EnvironmentAttribute;"
+                    )
             )
     )
-    private float getAmbientLight(float original) {
-        return should(RenderOptimiser.getFullBright()) ? 15.0f : original;
+    private Vector3f getAmbientLight(Vector3f original) {
+        return should(RenderOptimiser.getFullBright()) ? new Vector3f(15.0f) : original;
     }
 }
