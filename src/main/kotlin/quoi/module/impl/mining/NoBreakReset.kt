4ec9bae1d19cc4d6a70f2687b74049251a7a4f39
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-07-05 20:08:54 +0200

    feat: add no break reset

---
+++ src/main/kotlin/quoi/module/impl/mining/NoBreakReset.kt
@@ -0,0 +1,26 @@
+package quoi.module.impl.mining
+
+import net.minecraft.network.protocol.game.ClientboundContainerSetSlotPacket
+import quoi.api.events.PacketEvent
+import quoi.api.events.core.on
+import quoi.mixins.accessors.ItemInHandRendererAccessor
+import quoi.mixins.accessors.MultiPlayerGameModeAccessor
+import quoi.module.Module
+
+object NoBreakReset : Module(
+    "No Break Reset",
+    desc = "Prevents held item updates from resetting block breaking progress and the equip animation."
+) {
+    init {
+        on<PacketEvent.ReceivedPost, ClientboundContainerSetSlotPacket> {
+            if (!active || mc.screen != null) return@on
+
+            val slot = packet.slot
+            if (slot !in 36..44 || player.inventory.selectedSlot != slot - 36) return@on
+
+            val stack = packet.item
+            (gameMode as MultiPlayerGameModeAccessor).setDestroyingItem(stack)
+            (mc.entityRenderDispatcher.itemInHandRenderer as ItemInHandRendererAccessor).setMainHandItem(stack)
+        }
+    }
+}
