009fdfdc8659a4de9b28be9224f6055256948988
pigeonlover1998 <200670258+pigeonlover1998@users.noreply.github.com>
2026-07-07 01:08:36 +0300

    feat(slayers): boss esp

--- src/main/kotlin/quoi/module/impl/misc/slayers/Slayers.kt
+++ src/main/kotlin/quoi/module/impl/misc/slayers/Slayers.kt
@@ -1,9 +1,11 @@
 package quoi.module.impl.misc.slayers
 
+import net.fabricmc.fabric.api.client.rendering.v1.world.WorldRenderContext
 import net.minecraft.network.protocol.game.ClientboundSetPlayerTeamPacket
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.decoration.ArmorStand
 import quoi.api.colour.Colour
+import quoi.api.colour.alpha
 import quoi.api.colour.withAlpha
 import quoi.api.events.PacketEvent
 import quoi.api.events.RenderEvent
@@ -14,45 +16,43 @@ import quoi.api.events.core.trackedBy
 import quoi.api.skyblock.location.Island
 import quoi.module.Module
 import quoi.module.impl.misc.slayers.blaze.BlazeSlayer
-import quoi.module.impl.misc.slayers.blaze.Attunement
-import quoi.module.settings.UIComponent.Companion.childOf
-import quoi.module.settings.UIComponent.Companion.visibleIf
+import quoi.module.settings.group.SettingGroup.Companion.childOf
 import quoi.utils.EntityUtils.getEntities
 import quoi.utils.EntityUtils.getEntity
 import quoi.utils.EntityUtils.interpolatedBox
-import quoi.utils.EntityUtils.renderPos
 import quoi.utils.StringUtils.noControlCodes
 import quoi.utils.romanToInt
-import quoi.utils.render.drawStyledBox
-import quoi.utils.render.drawTracer
 
 @Suppress("unnecessary_safe_call")
 object Slayers : Module(
     "Slayers",
-    desc = "Utilities for Slayer quests, including boss ESP.",
     area = Island.Skyblock
 ) {
 
-    private val bossEsp by switch("Boss ESP", desc = "Highlights your active Slayer boss through walls.")
-    private val espStyle by selector("Style", "Filled box", arrayListOf("Box", "Filled box", "Filled"), desc = "Render style for Slayer bosses.").childOf(::bossEsp)
-    private val outlineColour by colourPicker("Outline colour", Colour.CYAN, allowAlpha = true, desc = "Outline colour for non-Blaze Slayer bosses.").childOf(::bossEsp).visibleIf { espStyle.selected != "Filled" }
-    private val fillColour by colourPicker("Fill colour", Colour.CYAN.withAlpha(0.33f), allowAlpha = true, desc = "Fill colour for non-Blaze Slayer bosses.").childOf(::bossEsp).visibleIf { espStyle.selected != "Box" }
-    private val tracer by switch("Tracer", desc = "Draws a line to your active Slayer boss.").childOf(::bossEsp)
-    private val depth by switch("Depth check", desc = "Hides the ESP behind blocks.").childOf(::bossEsp)
-
-    private val attunementColours = mapOf(
-        Attunement.ASHEN to Colour.BLACK,
-        Attunement.SPIRIT to Colour.WHITE,
-        Attunement.AURIC to Colour.YELLOW,
-        Attunement.CRYSTAL to Colour.CYAN
-    )
-
     private val slayers = setOf(
         BlazeSlayer
-    ).flatMap { it.features }
+    )
+
+    private val esp by switch("Boss ESP")
+    private val highlight = highlight(aabbOffset = true).childOf(::esp)
+    private val tracer = tracer(distance = null).childOf(::esp)
+
+    init {
+        on<RenderEvent.World> {
+            if (!esp || questState != QuestState.KILLING) return@on
+
+            ctx.drawSlayer(currentBoss)
+
+            slayers.forEach { slayer ->
+                slayer.entitiesForRender.forEach { (it, col) ->
+                    ctx.drawSlayer(it, col)
+                }
+            }
+        }
+    }
 
     override fun onDisable() {
-        slayers.forEach { it.onDisable() }
+        slayers.forEach { s -> s.features.forEach { it.onDisable() } }
         super.onDisable()
     }
 
@@ -67,7 +67,7 @@ object Slayers : Module(
             text.contains("Combat") || text.contains("Kills") -> QuestState.SPAWNING
             text == "Slay the boss!" -> QuestState.KILLING
             text == "Boss slain!" -> QuestState.SLAIN
-            else -> it // can get stuck when boss is slain and you collect the reward but doesn't really matter since I won't be using it prob.
+            else -> it // can get stuck when boss is slain, and you collect the reward but doesn't really matter since I won't be using it prob.
         }
 
         if (new != it) SlayerEvent.State(it, new).post()
@@ -98,28 +98,19 @@ object Slayers : Module(
             ?.let { (tier) -> romanToInt(tier) }
             ?: return@trackedBy null
 
-        val a = getEntity(spawnedBy.id - 3) as? LivingEntity
-        a
+        getEntity(spawnedBy.id - 3) as? LivingEntity
     }
 
-    init {
-        on<RenderEvent.World> {
-            if (!bossEsp || questState != QuestState.KILLING) return@on
+    private fun WorldRenderContext.drawSlayer(entity: LivingEntity?, overrideColour: Colour? = null) {
+        if (entity == null) return
+        highlight.draw(
+            this,
+            entity.interpolatedBox,
+            overrideColour = overrideColour,
+            overrideFillColour = overrideColour?.withAlpha(highlight.fill.alpha)
+        )
 
-            val targets = BlazeSlayer.espTargets()
-            if (targets.isNotEmpty()) {
-                targets.forEach { (entity, attunement) ->
-                    val colour = attunementColours[attunement] ?: Colour.CYAN
-                    ctx.drawStyledBox(espStyle.selected, entity.interpolatedBox, colour, colour.withAlpha(0.33f), depth = depth)
-                    if (tracer) ctx.drawTracer(entity.renderPos.add(0.0, entity.bbHeight / 2.0, 0.0), colour, depth = depth)
-                }
-                return@on
-            }
-
-            val boss = currentBoss?.takeUnless { it.isDeadOrDying } ?: return@on
-            ctx.drawStyledBox(espStyle.selected, boss.interpolatedBox, outlineColour, fillColour, depth = depth)
-            if (tracer) ctx.drawTracer(boss.renderPos.add(0.0, boss.bbHeight / 2.0, 0.0), outlineColour, depth = depth)
-        }
+        tracer.draw(this, entity, overrideColour = overrideColour)
     }
 
     private val tierRegex = Regex(".* (I{1,3}|IV|V) \\d+.*❤$")
