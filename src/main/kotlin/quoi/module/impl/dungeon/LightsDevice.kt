2f164fa3154d7cca71a23b1f1247ce1aba049e99
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-07-07 01:08:47 +0200

    fix(LightsDevice): delay setting always showing

--- src/main/kotlin/quoi/module/impl/dungeon/LightsDevice.kt
+++ src/main/kotlin/quoi/module/impl/dungeon/LightsDevice.kt
@@ -27,6 +27,7 @@ object LightsDevice : Module(
 ) {
     private val triggerBot by switch("Triggerbot", desc = "Automatically toggles the correct levers when you look at them.")
     private val delay by slider("Delay", 200L, 70L, 500L, 10L, unit = "ms", desc = "Delay between triggerbot clicks.")
+        .childOf(::triggerBot)
     private val hideUselessLevers by switch("Hide useless levers", desc = "Turns useless lights device levers into ghost blocks.")
 
     private var lastTriggerAt = 0L
