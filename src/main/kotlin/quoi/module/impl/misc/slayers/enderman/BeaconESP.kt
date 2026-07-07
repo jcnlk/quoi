0c54b31ccf9cb84d5b9873099fe0c46978541c77
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-07-07 23:37:48 +0200

    feat(Slayer): add eman beacon esp

---
+++ src/main/kotlin/quoi/module/impl/misc/slayers/enderman/BeaconESP.kt
@@ -0,0 +1,90 @@
+package quoi.module.impl.misc.slayers.enderman
+
+import net.minecraft.core.BlockPos
+import net.minecraft.network.protocol.game.ClientboundBlockUpdatePacket
+import net.minecraft.network.protocol.game.ClientboundSectionBlocksUpdatePacket
+import net.minecraft.world.entity.EquipmentSlot
+import net.minecraft.world.entity.decoration.ArmorStand
+import net.minecraft.world.entity.monster.EnderMan
+import net.minecraft.world.item.Items
+import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.phys.AABB
+import net.minecraft.world.phys.Vec3
+import quoi.api.colour.Colour
+import quoi.api.events.PacketEvent
+import quoi.api.events.RenderEvent
+import quoi.api.events.SlayerEvent
+import quoi.api.events.core.on
+import quoi.module.impl.misc.slayers.QuestState
+import quoi.module.impl.misc.slayers.Slayers
+import quoi.module.settings.group.ToggleableGroup
+import quoi.utils.EntityUtils.getEntities
+import quoi.utils.EntityUtils.interpolatedBox
+
+object BeaconESP : ToggleableGroup(
+    EndermanSlayer,
+    "Beacon ESP",
+    desc = "Highlights the Voidgloom Yang Glyph and draws a tracer to it."
+) {
+    private val highlight = highlight(
+        colour = Colour.RED,
+        fillColour = Colour.RED,
+        glow = false
+    )
+    private val tracer = tracer(colour = Colour.RED, distance = null)
+
+    private val placedBeacons = mutableMapOf<BlockPos, Long>()
+
+    init {
+        on<PacketEvent.Received> {
+            if (Slayers.questState != QuestState.KILLING) return@on
+            when (val packet = packet) {
+                is ClientboundBlockUpdatePacket -> updateBeacon(packet.pos, packet.blockState.block == Blocks.BEACON)
+                is ClientboundSectionBlocksUpdatePacket -> packet.runUpdates { pos, state ->
+                    updateBeacon(pos, state.block == Blocks.BEACON)
+                }
+            }
+        }
+
+        on<SlayerEvent.State> {
+            if (new != QuestState.KILLING) placedBeacons.clear()
+        }
+
+        on<RenderEvent.World> {
+            if (Slayers.questState != QuestState.KILLING) return@on
+
+            val now = System.currentTimeMillis()
+            placedBeacons.entries.removeIf { now - it.value > 7_000L }
+
+            getEntities<EnderMan>()
+                .filter { it.carriedBlock?.block == Blocks.BEACON }
+                .forEach { entity ->
+                    highlight.draw(ctx, entity.interpolatedBox)
+                    tracer.draw(ctx, entity)
+                }
+
+            getEntities<ArmorStand>()
+                .filter { it.getItemBySlot(EquipmentSlot.HEAD).item == Items.BEACON }
+                .forEach { entity ->
+                    highlight.draw(ctx, entity.interpolatedBox)
+                    tracer.draw(ctx, entity)
+                }
+
+            placedBeacons.keys.forEach { pos ->
+                highlight.draw(ctx, AABB(pos))
+                tracer.draw(ctx, Vec3.atCenterOf(pos))
+            }
+        }
+    }
+
+    private fun updateBeacon(pos: BlockPos, isBeacon: Boolean) {
+        val immutable = pos.immutable()
+        if (isBeacon) placedBeacons[immutable] = System.currentTimeMillis()
+        else placedBeacons.remove(immutable)
+    }
+
+    override fun onDisable() {
+        placedBeacons.clear()
+        super.onDisable()
+    }
+}
