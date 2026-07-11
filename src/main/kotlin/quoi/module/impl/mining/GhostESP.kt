50c7e5b57a5e140c4473bd9c72a9801e6557ae85
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-07-11 15:38:02 +0200

    feat(GhostESP): add hide ghost

--- src/main/kotlin/quoi/module/impl/mining/GhostESP.kt
+++ src/main/kotlin/quoi/module/impl/mining/GhostESP.kt
@@ -3,7 +3,6 @@ package quoi.module.impl.mining
 import net.minecraft.world.entity.monster.Creeper
 import quoi.api.colour.Colour
 import quoi.api.colour.withAlpha
-import quoi.api.events.EntityEvent
 import quoi.api.events.RenderEvent
 import quoi.api.events.core.on
 import quoi.api.skyblock.location.Island
@@ -11,35 +10,31 @@ import quoi.module.Module
 import quoi.utils.EntityUtils.getEntities
 import quoi.utils.EntityUtils.interpolatedBox
 
-/**
- * TODO:
- *  exclude wither cloak creepers
- */
-
 object GhostESP : Module(
     "Ghost ESP",
     area = Island.DwarvenMines,
     subarea = "The Mist"
 ) {
+    private val hideGhost by switch("Hide Ghost")
+
     private val highlight = highlight(
         colour = Colour.RGB(0, 200, 200),
         fillColour = Colour.RGB(0, 200, 200).withAlpha(0.5f),
     )
 
     init {
+        on<RenderEvent.Entity> {
+            if (hideGhost && entity is Creeper && isGhost(entity)) cancel()
+        }
+
         on<RenderEvent.World> {
             getEntities<Creeper>().filter(::isGhost).forEach { ghost ->
                 highlight.draw(ctx, ghost.interpolatedBox)
             }
         }
-
-        on<EntityEvent.ForceGlow> {
-            val ghost = entity as? Creeper ?: return@on
-            if (isGhost(ghost)) highlight.draw(this)
-        }
     }
 
     @JvmStatic
     fun isGhost(creeper: Creeper): Boolean =
-        creeper.y in 100.0..120.0 && creeper.isInvisible && creeper.isPowered
+        creeper.y in 76.0..100.0 && creeper.isInvisible && creeper.isPowered && creeper.health != 20f
 }
