17dab27936eca8aaf9019c98d3e778a7ea7e3cb6
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-07-16 17:40:34 +0200

    refactor(ClickGUI): clean up hud and visual settings

--- src/main/kotlin/quoi/module/impl/render/clickgui/ClickGui.kt
+++ src/main/kotlin/quoi/module/impl/render/clickgui/ClickGui.kt
@@ -53,7 +53,6 @@ import quoi.module.Module
 import quoi.module.ModuleManager
 import quoi.module.impl.misc.Test
 import quoi.module.impl.render.clickgui.impl.Data
-import quoi.module.impl.render.clickgui.impl.Displays
 import quoi.module.impl.render.clickgui.impl.PathSettings
 import quoi.module.impl.render.clickgui.impl.PrefixSettings
 import quoi.module.impl.render.clickgui.impl.VisualsSettings
@@ -88,7 +87,6 @@ object ClickGui : Module(
         VisualsSettings,
         PrefixSettings,
         PathSettings,
-        Displays,
         Data
     )
 
--- src/main/kotlin/quoi/module/impl/render/clickgui/impl/VisualsSettings.kt
+++ src/main/kotlin/quoi/module/impl/render/clickgui/impl/VisualsSettings.kt
@@ -10,38 +10,21 @@ import quoi.utils.ui.rendering.NVGRenderer
 
 object VisualsSettings : SettingGroup(
     ClickGui,
-    SelectorComponent("Visuals", "Light", arrayListOf("Light", "Dark", "Onyx"))//.open()
+    SelectorComponent("Visuals", "Dark", arrayListOf("Light", "Dark", "Onyx"))//.open()
         .onValueChanged { _, _ ->
             reopen()
         }
 ) {
     val selectedTheme get() = (component as SelectorComponent<*>).selected
-    val colour by colourPicker("Colour", Colour.RGB(255, 204, 134)).asParent()
-    val classColors by selector(
-        "Class colors",
-        "Hypixel Style",
-        listOf("Hypixel Style", "Noamm Style"),
-        desc = "Blame Noamm for this"
-    )
-    val moduleSorting by selector("Module sorting", ModuleSorting.Alphabetical).onValueChanged { _, _ -> reopen() }
+    val colour by colourPicker("Colour", Colour.CYAN).asParent()
+    val moduleSorting by selector("Module sorting", ModuleSorting.Longest).onValueChanged { _, _ -> reopen() }
 
     var rainbowSpeed by slider("Rainbow colour speed", 0.5f, 0.05f, 5.0f, 0.05f)
 
-
     @Suppress("unused")
-    enum class ModuleSorting(
-        val comparator: Comparator<Module>
-    ) {
-        WidthDescending(
-            compareByDescending<Module> { NVGRenderer.textWidth(it.name, 18f, NVGRenderer.defaultFont) }.thenBy { it.name.lowercase() }
-        ),
-
-        WidthAscending(
-            compareBy<Module> { NVGRenderer.textWidth(it.name, 18f, NVGRenderer.defaultFont) }.thenBy { it.name.lowercase() }
-        ),
-
-        Alphabetical(
-            compareBy<Module> { it.name.lowercase() }
-        );
+    enum class ModuleSorting(val comparator: Comparator<Module>) {
+        Longest(compareByDescending<Module> { NVGRenderer.textWidth(it.name, 18f, NVGRenderer.defaultFont) }.thenBy { it.name.lowercase() }),
+        Shortest(compareBy<Module> { NVGRenderer.textWidth(it.name, 18f, NVGRenderer.defaultFont) }.thenBy { it.name.lowercase() }),
+        Alphabetical(compareBy<Module> { it.name.lowercase() });
     }
 }
