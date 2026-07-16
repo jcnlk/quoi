2721e648204f7742d8f8993f23b6ba2b54a8306d
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-07-16 18:33:03 +0200

    fix(HidePlayers): click through player not working properly

--- src/main/java/quoi/mixins/LocalPlayerMixin.java
+++ src/main/java/quoi/mixins/LocalPlayerMixin.java
@@ -6,13 +6,18 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import quoi.api.events.EntityEvent;
 import quoi.api.world.Direction;
 import quoi.module.impl.misc.ItemAnimations;
 import quoi.utils.skyblock.player.RotationUtils;
+
+import java.util.function.Predicate;
 
 @Mixin(LocalPlayer.class)
 public class LocalPlayerMixin extends AbstractClientPlayer {
@@ -53,5 +58,32 @@ public class LocalPlayerMixin extends AbstractClientPlayer {
         Direction dir = RotationUtils.getServerDirection();
         if (dir == null) return original;
         return dir.getPitch();
+    }
+
+    @ModifyExpressionValue(
+            method = "raycastHitResult",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/world/entity/EntitySelector;CAN_BE_PICKED:Ljava/util/function/Predicate;"
+            )
+    )
+    private Predicate<Entity> filterAttackRangePick(Predicate<Entity> original) {
+        return filterPickableEntities(original);
+    }
+
+    @ModifyExpressionValue(
+            method = "pick",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/world/entity/EntitySelector;CAN_BE_PICKED:Ljava/util/function/Predicate;"
+            )
+    )
+    private static Predicate<Entity> filterStandardPick(Predicate<Entity> original) {
+        return filterPickableEntities(original);
+    }
+
+    @Unique
+    private static Predicate<Entity> filterPickableEntities(Predicate<Entity> original) {
+        return entity -> original.test(entity) && !new EntityEvent.Pick(entity).post();
     }
 }
