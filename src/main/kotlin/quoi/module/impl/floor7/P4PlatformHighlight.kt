5f50fb7a8aee955f7a0130b1ca5c5dac9fc0c8a4
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-08-02 21:17:15 +0200

    refactor(P4PlatformHighlight): use highlight settings

---
+++ src/main/kotlin/quoi/module/impl/floor7/P4PlatformHighlight.kt
@@ -0,0 +1,34 @@
+package quoi.module.impl.floor7
+
+import net.minecraft.world.phys.AABB
+import quoi.api.colour.Colour
+import quoi.api.colour.withAlpha
+import quoi.api.events.RenderEvent
+import quoi.api.events.core.on
+import quoi.api.skyblock.dungeon.Floor7Utils
+import quoi.api.skyblock.dungeon.Stage
+import quoi.api.skyblock.location.Island
+import quoi.api.skyblock.location.invoke
+import quoi.module.Module
+
+object P4PlatformHighlight : Module(
+    "P4 Platform Highlight",
+    desc = "Highlights 3x3 area to mine after Goldor dies.",
+    area = Island.Dungeon(7, inBoss = true)
+) {
+    private val platformHighlight = highlight(
+        colour = Colour.CYAN,
+        fillColour = Colour.CYAN.withAlpha(60),
+        glow = false,
+        defaultStyle = "Filled",
+    )
+
+    private val healerBox = AABB(53.0, 63.0, 113.0, 56.0, 64.0, 116.0)
+
+    init {
+        on<RenderEvent.World> {
+            if (!Floor7Utils.inStage(Stage.S5)) return@on
+            platformHighlight.draw(ctx, healerBox)
+        }
+    }
+}
