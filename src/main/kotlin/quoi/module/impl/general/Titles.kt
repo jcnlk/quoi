c5df5b16484e7bc693d78a866d6fc9255bb5ed8c
pigeonlover1998 <200670258+pigeonlover1998@users.noreply.github.com>
2026-08-06 19:53:59 +0300

    fix(secrets): sound setting names

--- src/main/kotlin/quoi/module/impl/general/Titles.kt
+++ src/main/kotlin/quoi/module/impl/general/Titles.kt
@@ -23,7 +23,7 @@ object Titles : Module("Titles", desc = "temp module") {
     private val titleDuration by slider("Title duration", 2.0, 0.5, 5.0, 0.1, desc = "How long the title stays on screen.", "s").childOf(::titleSettings)
 
     private val playSound by switch("Play sound", desc = "Plays a sound when title pops up").childOf(::titleSettings)
-    private val soundSettings = sound("Title").childOf(::titleSettings) { playSound }
+    private val soundSettings = sound("Title sound").childOf(::titleSettings) { playSound }
 
     init {
         on<ChatEvent.Packet> {
