464acd548eae9137375da237fbfebd4814a2abae
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-08-06 17:14:59 +0200

    feat: readd wither cloak

--- src/main/kotlin/quoi/module/ModuleManager.kt
+++ src/main/kotlin/quoi/module/ModuleManager.kt
@@ -63,6 +63,7 @@ object ModuleManager : EventListener {
             TickTimers,
             AutoInvincibility,
             InvincibilityTimer,
+            WitherCloak,
 
             // MISC
             Test,
