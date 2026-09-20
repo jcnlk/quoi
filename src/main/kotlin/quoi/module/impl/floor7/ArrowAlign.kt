65071cc02a5aa33b8e26c2be3cce33d97f2a54b2
jcnlk <84159719+jcnlk@users.noreply.github.com>
2026-09-20 17:01:43 +0200

    fix(ArrowAlign): prevent duplicate interactions

--- src/main/kotlin/quoi/module/impl/floor7/ArrowAlign.kt
+++ src/main/kotlin/quoi/module/impl/floor7/ArrowAlign.kt
@@ -214,10 +214,6 @@ object ArrowAlign : Module(
         mc.connection?.send(
             ServerboundInteractPacket(frame.entity.id, InteractionHand.MAIN_HAND, Vec3(0.03125, 0.0, 0.0), player.isShiftKeyDown)
         )
-
-        mc.connection?.send(
-            ServerboundInteractPacket(frame.entity.id, InteractionHand.MAIN_HAND, Vec3.ZERO, player.isShiftKeyDown)
-        )
     }
 
     private fun getFrameIndex(pos: BlockPos): Int {
